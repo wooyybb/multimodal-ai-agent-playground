5918539c6f75dfa1c21f936705ed66783530bfae
wooyybb <woo03187@naver.com>
2026-06-27 14:16:36 +0900

    Connect VisionAgent and PromptAgent pipeline

--- workflow/pipeline.py
+++ workflow/pipeline.py
@@ -1,3 +1,19 @@
-def run_pipeline():
-    """TODO"""
-    pass
+from agents.prompt_agent import PromptAgent
+from agents.vision_agent import VisionAgent
+
+
+class MultimodalPipeline:
+    def __init__(self):
+        self.vision_agent = VisionAgent()
+        self.prompt_agent = PromptAgent()
+
+    def run(self, image, user_prompt):
+        print("[Pipeline] Starting pipeline...")
+        caption = self.vision_agent.run(image)
+        final_prompt = self.prompt_agent.run(caption, user_prompt)
+        print("[Pipeline] Finished pipeline.")
+
+        return {
+            "caption": caption,
+            "final_prompt": final_prompt,
+        }
